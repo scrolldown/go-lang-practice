89afdcc28ada299e82468f7e70c69b3f3019fef1
scrolldown <scrolldown@naver.com>
2022-02-09 18:13:11 +0900

    learn 1 : type of go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/scrolldown/go-lang-practice/something"
+)
+
+func main() {
+	var name string = "JH"
+	name2 := "JH2"
+
+	fmt.Println("Hello")
+	something.SayBye()
+	fmt.Println(name)
+	fmt.Println(name2)
+}
---
+++ something/something.go
@@ -0,0 +1,11 @@
+package something
+
+import "fmt"
+
+func sayHello() {
+	fmt.Println("Hello something")
+}
+
+func SayBye() {
+	fmt.Println("Bye something")
+}
